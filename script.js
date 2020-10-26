c0ca29e622e2f3082320b7ede7bc50cc471c1b8b
kuiatchou merveille <kume0804@student.iepscf-uccle.be>
2020-10-26 18:55:14 +0100

    fin tp7

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Écrivez votre code JavaScript ici.
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const addItem = document.querySelector("button");
+/*const delet = document.createElement("button");
+bouton.addEventListener("click", ajoute());*/
+
+addItem.addEventListener("click", ajoute);
+
+  function ajoute(){
+  const maVariable = input.value;
+  input.value = "";
+  const sousList = document.createElement("li");
+  const span = document.createElement("span");
+  const dellete = document.createElement("button");
+  sousList.appendChild(span);
+  sousList.appendChild(dellete);
+  span.textContent = maVariable;
+  dellete.textContent = 'delete';
+  list.appendChild(sousList);
+
+  dellete.onclick = function () {
+    list.removeChild(sousList);
+    input.focus();
+  }
+
+  input.focus();
+
+}
+
+
+
